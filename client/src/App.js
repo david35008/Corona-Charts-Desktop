e2d6d4296481c583149bc1269efb9591688a8a9a
david35008 <67857450+david35008@users.noreply.github.com>
2020-10-26 19:38:36 +0200

    finish part 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,51 @@
+// import { LineChart, Legend, Tooltip, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+import Chart from './components/Chart';
+
 function App() {
+//   const data = [
+//     {date: '01.01.2020', uv: 400, pv: 700, x: 100}, 
+//   {date: '02.01.2020', uv: 500, pv: 800, x: 200},
+//   {date: '03.01.2020', uv: 700, pv: 900, x: 300},
+// ];
+
+// const covid = [
+//   {date: '01.01.2020', sick: 400, pv: 700, x: 100}, 
+// {date: '02.01.2020', sick: 500, pv: 800, x: 200},
+// {date: '03.01.2020', sick: 700, pv: 900, x: 300},
+// ];
+
+// [
+//   {date:'01.01.2020', sick:5},
+//   {date:'01.01.2020', sick:5},
+//   {date:'01.01.2020', sick:5},
+//   {date:'01.01.2020', sick:5},
+//   {date:'01.01.2020', sick:5},
+//   {date:'01.01.2020', sick:5},
+//   {date:'01.01.2020', sick:5},
+// ]
+
   return (
     <div className="App">
-      Hello World
+      <Chart />
+    {/* <LineChart width={600} height={300} data={data}>
+        <Line type="linear" dataKey="uv" stroke="red" />
+        <Line type="monotone" dataKey="pv" stroke="yellow" />
+        <Line type="monotone" dataKey="x" stroke="#000000" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+
+
+      <LineChart width={600} height={300} data={covid}>
+        <Line type="linear" dataKey="sick" stroke="red" />
+        <CartesianGrid stroke="#ccc" />
+        <XAxis dataKey="date" />
+        <YAxis />
+        <Tooltip />
+        <Legend dataKey="Confirmed Cases"/>
+      </LineChart> */}
     </div>
   );
 }
